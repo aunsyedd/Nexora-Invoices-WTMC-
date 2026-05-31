fd5c058576e5efdb9b6e01e197c6a92e572f3696
Syed Aun <auns5757@gmail.com>
2026-05-31 15:22:20 +0500

    Initial project commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,14 @@
 export default function Footer() {
   return (
     <div className="p-4 border-t border-gray-200 text-center text-xs text-gray-500">
-      Engineered and developed by{" "}
+      Engineered and developed by {" "}
       <a
         href="https://www.nexoratech.info/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-600 hover:underline font-medium"
       >
+        <br></br>
         Nexora Tech
       </a>
     </div>
